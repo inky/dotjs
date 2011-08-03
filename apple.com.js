a56ba915bebdf1c114de93fbad30e2a5b98be231
Liam Cooke <liamcooke@gmail.com>
2011-08-03 01:21:20 +0100

    Steve Jobs Office Assistant

---
+++ apple.com.js
@@ -0,0 +1,18 @@
+var steve = $('<div><img src="http://media.tumblr.com/tumblr_lpbs43jgcx1qz5urdo1_100.png"/></div>');
+steve.css({
+    position: 'fixed',
+    right: 5,
+    bottom: 20,
+    zIndex: 9001,
+    width: 136,
+    height: 136,
+    textAlign: 'center'
+});
+
+$('<style>@-webkit-keyframes spinSteve { ' +
+  'from { -webkit-transform: rotate(0deg); } ' +
+  'to { -webkit-transform: rotate(-360deg); } ' +
+  '}</style>').appendTo(document.body);
+
+steve[0].style['-webkit-animation'] = 'spinSteve 2s linear infinite';
+steve.appendTo(document.body);
