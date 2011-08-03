2ef821d8a80d8650eb6a8424000e3398223c54d9
Liam Cooke <liamcooke@gmail.com>
2011-08-03 02:53:16 +0100

    Toggle suggestions on YouTube

--- defunkt.io.js
+++
@@ -1,4 +0,0 @@
-if (window.location.pathname == '/dotjs/') {
-    $('#content').css({ background: '#fff', padding: '1em' });
-    $('body').css({ backgroundImage: 'url(http://bit.ly/ghD24e)' });
-}
--- youtube.com.js
+++ youtube.com.js
@@ -1,1 +1,19 @@
 $('.ad-div').css('visibility', 'hidden');
+
+$('.watch-module-body').each(function() {
+    var related = $('.video-list, #watch-more-related-button', this),
+        hidden = false,
+        bg = '#fff', bgHover = '#d1e1fa',
+        border = '#eaeaea', borderHover = '#b6d0f7';
+    $('h4', this).click(function() {
+        related.css('visibility', (hidden) ? 'visible' : 'hidden');
+        hidden = !hidden;
+    }).css({
+        background: '#fff',
+        border: '1px solid #eaeaea',
+        borderRadius: '4px',
+        cursor: 'pointer',
+        display: 'table',
+        padding: '0.5em'
+    });
+});
