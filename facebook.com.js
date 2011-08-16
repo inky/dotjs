ab138a40dd7b8479e8f77c5c60c9ec6d69443292
Liam Cooke <liamcooke@gmail.com>
2011-08-16 04:15:44 +0100

    Hide Facebook junk

---
+++ facebook.com.js
@@ -0,0 +1,9 @@
+// hide ads
+// based on https://github.com/mraichelson/dotJS/blob/master/facebook.com.js
+$('.ego_section').css({
+    opacity: 0,
+    overflow: 'hidden',
+    height: 0
+});
+
+$('#pagelet_friends_online .isOffline').hide();
